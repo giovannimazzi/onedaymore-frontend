b538450d3d0051a866b68927be20e1b4760ebb1d
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-31 20:11:29 +0200

    adjust on cards for details

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -48,6 +48,8 @@ export default function HomePage() {
                 alt="Cibo"
               />
               <div className="card-body">
+                <span class="badge bg-success p-2 mb-3">Best seller</span>
+                <h5 className="h3 mb-3">€50,00</h5>
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
@@ -65,6 +67,8 @@ export default function HomePage() {
                 alt="..."
               />
               <div className="card-body">
+                <span class="badge bg-success p-2 mb-3">Best seller</span>
+                <h5 className="h3 mb-3">€50,00</h5>
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
@@ -82,16 +86,24 @@ export default function HomePage() {
                 alt="..."
               />
               <div className="card-body">
+                <span class="badge bg-success p-2 mb-3">Best seller</span>
+                <h5 className="h3 mb-3">€50,00</h5>
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card’s content.
                 </p>
+
                 <a href="#" className="btn btn-primary mb-4">
                   Vai al prodotto
                 </a>
               </div>
             </div>
+          </div>
+          <div className="text-center">
+            <button className="btn btn-success text-center py-3" type="button">
+              Visualizza tutti i prodotti
+            </button>
           </div>
         </div>
       </div>
