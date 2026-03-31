4d1b325faeb1f954c32f2a54b76ec2bb7b8894b8
Salvo-Raia <salvo.raia@outlook.it>
2026-03-31 17:10:38 +0200

    Add: herospace (to customize)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,22 @@
 export default function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <section className="hero p-4 d-flex flex-column justify-content-between align-items-start">
+        <div>
+          <h1>Titolo Hero provvisorio</h1>
+          <p>Sottotitolo hero provvisorio</p>
+        </div>
+        <button className="btn btn-primary">Call To Action</button>
+      </section>
+
+      <section className="splash-products">
+        <div>
+          <div className="row row-cols-2">
+            <div className="col"></div>
+            <div className="col"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
