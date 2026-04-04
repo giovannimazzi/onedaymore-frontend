cde4b21f08ba83424e7bdd8c90062457109c9a40
Salvo-Raia <salvo.raia@outlook.it>
2026-04-04 20:16:35 +0200

    add: categoryIconHandler to new arrivals products on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -148,6 +148,7 @@ export default function HomePage() {
                   key={product.id}
                   productName={product.name}
                   productImage={product.image_url}
+                  productCategoryId={product.category_id}
                   productCategorySlug={product.category_slug}
                   productPrice={product.price}
                   productShortDescr={
